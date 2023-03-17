e83d349ebff02aefda149b9401ccbeb02d173a22
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-03-17 16:54:41 -0500

    buttonsActions update
    - added function that sends the user to the home page whenever they click the logo

--- assets/buttonsActions.js
+++ assets/buttonsActions.js
@@ -29,6 +29,16 @@ menuButtons.forEach((index) => {
 
 // MENU BUTTONS ACTION //
 
+// LOGO TAKES YOU TO HOME PAGE //
+
+const logo = document.querySelector('#menuLogo');
+logo.addEventListener('click', ()=> {
+  heroPage.classList.remove('switch');
+  aboutPage.classList.add('switch');
+});
+
+// LOGO TAKES YOU TO HOME PAGE //
+
 // ESCAPE ClOSES EVERYTHING //
 
 document.addEventListener('keydown', (e) => {
